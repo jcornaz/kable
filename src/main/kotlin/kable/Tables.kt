580811c57cea6dcf228e8ae3f79392f0ef15dd9f
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2017-03-29 22:09:57 +0200

    Declare return type for the functions tableOf

--- src/main/kotlin/kable/Tables.kt
+++ src/main/kotlin/kable/Tables.kt
@@ -9,10 +9,10 @@ fun <R, C, V> emptyTable(): Table<R, C, V> = BiKeyMap()
 fun <R, C, V> entry(row: R, column: C, value: V): Table.Entry<R, C, V> = SimpleTableEntry(row, column, value)
 
 /** Create a table with the given entries */
-fun <R, C, V> tableOf(entries: Collection<Table.Entry<R, C, V>>) = BiKeyMap(entries)
+fun <R, C, V> tableOf(entries: Collection<Table.Entry<R, C, V>>): Table<R, C, V> = BiKeyMap(entries)
 
 /** Create a table with the given entries */
-fun <R, C, V> tableOf(vararg entries: Table.Entry<R, C, V>) = tableOf(entries.toList())
+fun <R, C, V> tableOf(vararg entries: Table.Entry<R, C, V>): Table<R, C, V> = tableOf(entries.toList())
 
 /** Return true if, and only if, the table contains the given row-column key pair */
 operator fun <R, C> Table<R, C, *>.contains(key: Pair<R, C>) = contains(key.first, key.second)
