e9d5cf11a4f2d061ca7f675e653f5217dcc9fe5a
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2017-03-29 22:17:46 +0200

    Add map-like extensions functions
    Concern : #3
    
    Functions added :
    * forEach
    * all
    * any
    * none
    * count
    * mayBy / maxWith
    * minBy / minWith

--- src/main/kotlin/kable/Tables.kt
+++ src/main/kotlin/kable/Tables.kt
@@ -2,6 +2,8 @@
 
 package kable
 
+import java.util.*
+
 /** Create an empty table */
 fun <R, C, V> emptyTable(): Table<R, C, V> = BiKeyMap()
 
@@ -40,3 +42,48 @@ fun <R, C, V> Table<R, C, V>.minusColumn(column: C): Table<R, C, V> =
 /** Return a new table with the same entries except the row-column pair */
 operator fun <R, C, V> Table<R, C, V>.minus(key: Pair<R, C>): Table<R, C, V> =
         if (key !in this) this else BiKeyMap(entries.filterNot { it.row == key.first && it.column == key.second })
+
+/** Performs the given [action] on each entry */
+inline fun <R, C, V> Table<R, C, V>.forEach(action: (Table.Entry<R, C, V>) -> Unit): Unit =
+        entries.forEach(action)
+
+/** Returns `true` if all entries match the given [predicate] */
+inline fun <R, C, V> Table<R, C, V>.all(predicate: (Table.Entry<R, C, V>) -> Boolean): Boolean =
+        entries.all(predicate)
+
+/** Returns `true` if map has at least one entry */
+fun Table<*, *, *>.any(): Boolean = entries.any()
+
+/** Returns `true` if at least one entry matches the given [predicate] */
+inline fun <R, C, V> Table<R, C, V>.any(predicate: (Table.Entry<R, C, V>) -> Boolean): Boolean =
+        entries.any(predicate)
+
+/** Returns `true` if the map has no entries */
+fun Table<*, *, *>.none(): Boolean = entries.none()
+
+/** Returns `true` if no entry match the given [predicate] */
+inline fun <R, C, V> Table<R, C, V>.none(predicate: (Table.Entry<R, C, V>) -> Boolean): Boolean =
+        entries.none(predicate)
+
+/** Returns the number of entries in this map */
+fun Table<*, *, *>.count(): Int = entries.count()
+
+/** Returns the number of entries matching the given [predicate] */
+inline fun <R, C, V> Table<R, C, V>.count(predicate: (Table.Entry<R, C, V>) -> Boolean): Int =
+        entries.count(predicate)
+
+/** Returns the first entry yielding the largest value of the given function or `null` if there are no entries */
+inline fun <R, C, V, T : Comparable<T>> Table<R, C, V>.maxBy(selector: (Table.Entry<R, C, V>) -> T): Table.Entry<R, C, V>? =
+        entries.maxBy(selector)
+
+/** Returns the first entry having the largest value according to the provided [comparator] or `null` if there are no entries */
+fun <R, C, V> Table<R, C, V>.maxWith(comparator: Comparator<Table.Entry<R, C, V>>): Table.Entry<R, C, V>? =
+        entries.maxWith(comparator)
+
+/** Returns the first entry yielding the smallest value of the given function or `null` if there are no entries */
+inline fun <R, C, V, T : Comparable<T>> Table<R, C, V>.minBy(selector: (Table.Entry<R, C, V>) -> T): Table.Entry<R, C, V>? =
+        entries.minBy(selector)
+
+/** Returns the first entry having the smallest value according to the provided [comparator] or `null` if there are no entries */
+fun <R, C, V> Table<R, C, V>.minWith(comparator: Comparator<Table.Entry<R, C, V>>): Table.Entry<R, C, V>? =
+        entries.minWith(comparator)
